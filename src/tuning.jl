8c0b5f62bf2d72c77a6e36a6d428b5a03f6d5433
lhnguyen-vn <lhnguyen.vn@gmail.com>
2021-06-20 18:02:25 -0400

    Add MLJTuning interface methods

--- src/tuning.jl
+++ src/tuning.jl
@@ -1,3 +1,26 @@
-function setup(tuning::ParticleSwarm, model, range, n, verbosity)
-    return initialize(tuning, range)
+function MLJTuning.setup(tuning::ParticleSwarm, model, range, n, verbosity)
+    return initialize(range, tuning)
+end
+
+function MLJTuning.models(
+    tuning::ParticleSwarm,
+    model,
+    history,
+    state,
+    n_remaining,
+    verbosity
+)
+    pbest!(state, tuning, last(history).measurement[1])
+    gbest!(state, tuning)
+    move!(state, tuning)
+    retrieve!(state, tuning)
+    fields = getindex.(state.ranges, :field)
+    new_models = map(1:ps.n_particles) do i
+        clone = deepcopy(model)
+        for (field, param) in zip(fields, getindex.(state.parameters, i))
+            recursive_setproperty!(clone, field, param)
+        end
+        clone
+    end
+    return new_models, state
 end
