fa522dff780996279010a5cd4e4c210e585ce9d0
lhnguyen-vn <lhnguyen.vn@gmail.com>
2021-07-28 00:30:56 -0400

    Add AdaptiveParticleSwarm strategy

--- src/MLJParticleSwarmOptimization.jl
+++ src/MLJParticleSwarmOptimization.jl
@@ -6,11 +6,12 @@ using Distributions
 using MLJBase
 using MLJTuning
 
-export StaticCoeffs, ParticleSwarm
+export ParticleSwarm, AdaptiveParticleSwarm
 
 include("swarm.jl")
 include("parameters.jl")
 include("update.jl")
 include("strategies/basic.jl")
+include("strategies/adaptive.jl")
 
 end
